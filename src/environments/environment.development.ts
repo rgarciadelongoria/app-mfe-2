6e467ad9f0d33079c529a41e78dfc8189ea41a54
Rubén García de Longoria <r.gdelongoria@crowdfarming.com>
2024-12-19 08:19:59 +0100

    feat: remove unused environment props

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ import { Environment } from "@interfaces/environment.interface";
 export const environment:Environment = {
     name: 'dev',
     web: {
-        host: 'http://192.168.100.127:4201'
+        host: 'http://192.168.1.40:4201'
     },
 }
